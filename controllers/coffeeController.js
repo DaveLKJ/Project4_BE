24f19ae87495fac84cf4ebc40280546c5913fc36
KongJian <kongjian3@hotmail.com>
2024-06-17 21:04:24 +0800

    update coffeecontroller

--- controllers/coffeeController.js
+++ controllers/coffeeController.js
@@ -4,7 +4,22 @@ import fs from "fs";
 // add coffee
 
 const addCoffee = async (req, res) => {
-    
+  let image_filename = `${req.file.filename}`;
+
+  const coffee = new coffeeModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await coffee.save();
+    res.json({ success: true, message: "Coffee added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 export { addCoffee };
