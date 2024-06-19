8564d0391487ca2ef25c3d42da63b7f2a271d0b5
KongJian <kongjian3@hotmail.com>
2024-06-19 22:51:51 +0800

    add: orders route and controller

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 
 import coffeeRouter from "./routes/coffeeRoute.js";
 import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
@@ -20,6 +22,8 @@ connectDB();
 app.use("/api/coffee", coffeeRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
