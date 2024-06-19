31350b0b7414ceb7d6ac3b4dbc58945d97769cb7
KongJian <kongjian3@hotmail.com>
2024-06-20 01:03:39 +0800

    update port

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("API working");
 });
 
-const PORT = 6001;
+const PORT = process.env.PORT || 6001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
